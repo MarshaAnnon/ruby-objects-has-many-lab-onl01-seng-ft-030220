d508f862d9c0dcf1ed2322c3ef5ef8f029f56179
Marsha Annon <marshaannon@gmail.com>
2020-03-16 21:20:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 require "pry"
 
 class Artist
+
+  @@song_count = 0
   attr_accessor :name
 
   def initialize(name)
@@ -10,9 +12,13 @@ class Artist
 
   def add_song(song)
     @songs << song
+    @@song_count += 1
   end
 
   def songs
     @songs
   end
+
+  def song_count
+    @@song_count
 end
