6b7a3efbdc4ee99b4c61e35a3e90e5d63f02792f
Marsha Annon <marshaannon@gmail.com>
2020-03-16 21:40:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,23 @@ require "pry"
 
 class Song
 
+  attr_accessor :artist, :name, :genre
+  @@all = []
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+    save
+  end
+
+  def save
+    @@all <<self
+  end
+
+  def save.all
+    @@all
+  end
+
+  end
+
 end
