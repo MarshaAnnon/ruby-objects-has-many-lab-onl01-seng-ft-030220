9a88878140d5178cc21d79a1dc84d18e97ece9b6
Marsha Annon <marshaannon@gmail.com>
2020-03-16 23:16:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,10 +14,14 @@ class Author
   def add_post(post)
     post.author = self
     @@post_count += 1
-
   end
-  def post
+  
+  def posts
     Post.all.select { | post | post.author == self}
+  end
+
+  def songs
+    Song.all.select { | song | song.artist == self}
   end
 
   def add_post_by_title(title)
